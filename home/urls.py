f66858034f7fd1946f88b65cdceedb3485e74294
ankeshkr <32651031+ankeshkr@users.noreply.github.com>
2020-10-07 00:38:33 +0530

    Update urls.py
    added login and logout

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('about', views.about, name='about'),
     path('catalogue', views.catalogue, name='services'),
     path('contact', views.contact, name='contact'),
+    ## Add
+    path('login', views.loginUser, name="login"),
+    path('logout', views.logoutUser, name="logout")
 ]
